a12e41ac195b41f097c85e4bf479334a355268ca
Shih-Chiang Chien <polo.hellfire@gmail.com>
2018-06-29 14:15:26 +0800

    readline from stdin in c++

---
+++ cxx/io/readline.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string line;
+
+  while (cin >> line) {
+    cout << line << '\n';
+    if (cin.eof()) break;
+  }
+
+  return 0;
+}
